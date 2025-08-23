908fafea7f47882e1c551877569517ccf23e2e2c
Amr Ibrahim <amr.ibrahim00@gmail.com>
2025-08-24 02:13:49 +0300

    Update iam.tf

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -18,6 +18,34 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+# Additional ECR policy for authentication and pulling
+resource "aws_iam_policy" "ecr_pull_policy" {
+  name        = "${var.project}-${var.environment}-ecr-pull-policy"
+  description = "Policy to allow ECR authentication and image pulling"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_pull" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_pull_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
