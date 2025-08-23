424407c694ab4a6fd157999bcd67c6462856c398
Amr Ibrahim <amr.ibrahim00@gmail.com>
2025-08-24 01:27:31 +0300

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,15 +1,29 @@
 output "instance_public_ip" {
-  value = aws_instance.app.public_ip
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_instance.public_ip
 }
 
 output "application_https_url" {
-  value = "https://${aws_instance.app.public_ip}"
+  description = "HTTPS URL for the application"
+  value       = "https://${aws_instance.app_instance.public_ip}"
+}
+
+output "application_http_url" {
+  description = "HTTP URL for the application (port 8080 - nginx reverse proxy)"
+  value       = "http://${aws_instance.app_instance.public_ip}:8080"
+}
+
+output "application_direct_url" {
+  description = "Direct HTTP URL for the application (port 80)"
+  value       = "http://${aws_instance.app_instance.public_ip}"
 }
 
 output "ecr_api_repo_url" {
-  value = aws_ecr_repository.api.repository_url
+  description = "URL of the ECR repository for the API"
+  value       = aws_ecr_repository.api.repository_url
 }
 
 output "ecr_web_repo_url" {
-  value = aws_ecr_repository.web.repository_url
+  description = "URL of the ECR repository for the web frontend"
+  value       = aws_ecr_repository.web.repository_url
 }
